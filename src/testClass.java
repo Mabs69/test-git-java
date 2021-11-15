782fed1ea3bd00ad874cce698474429e9c24a87a
mavs <mavs@email.com>
2021-11-15 14:51:08 +0800

    initial

---
+++ src/testClass.java
@@ -0,0 +1,18 @@
+
+public class testClass {
+    
+    public void xd () {
+        
+    }
+    
+    public void ssss () {
+        
+    }
+    public static void main(String [] args) {
+        boolean fak3rdYear = true;
+    }
+    
+    public void watda() {
+        
+    }
+}
